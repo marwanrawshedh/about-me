48f60faba11d0025e6746425fc8776f157b2d4df
marwanrawshedh <marwansalem55@yahoo.com>
2021-06-21 17:06:53 +0300

    addfiles

---
+++ js/app.js
@@ -0,0 +1,58 @@
+'use strict';
+//ert('i am marwan rawashdeh welcome to the guessing game page');
+//alert('If you are a Bollywood fan, leave this page');
+//alert('we just joking with you have a nice time');
+//alert('Please answer using Yes, No If you answer the correct answer, you will get a star');
+
+
+let first= prompt("do you like watching movies");
+
+
+
+switch(first.toLowerCase()) {
+    case "yes":alert('your answer is correct you will get first star');
+    document.write('<img src="https://image.pngaaa.com/369/1372369-middle.png" >');
+      break;
+      case "no":alert('your answer is false you will not get star');}
+      
+     console.log(first);
+     
+     let sec= prompt('is leonardo dicaprio my favorit actor in hollywood');
+     switch(sec.toLowerCase()) {
+        case "yes":alert('your answer is true you will get star');
+        document.write('<img src="https://image.pngaaa.com/369/1372369-middle.png" >');
+         break;
+        case "no":alert('your answer is false you will not get star');}
+          
+     
+     console.log(sec);
+     
+     let thir= prompt('did you watch inception or the wolf of wall streat?');
+     switch(thir.toLowerCase()) {
+        case "yes":alert('your answer is true you will get star');
+        document.write('<img src="https://image.pngaaa.com/369/1372369-middle.png" >');  
+        break;
+        case "no":alert('your answer is false you will not get star');}
+          
+     
+     console.log(thir);
+     
+     let forth= prompt('is jennifer lawrence better than margot robbie?');
+     switch(forth.toLowerCase()) {
+        case "yes":alert('your answer is true you will get star');
+        document.write('<img src="https://image.pngaaa.com/369/1372369-middle.png" >');
+        break;
+        case "no":alert('your answer is false you will not get star');}
+          
+     
+     console.log(forth);
+     
+     let fifth=prompt('Is 2015 the best year for movies in the new century?')
+     switch(fifth.toLowerCase()) {
+        case "no":alert('your answer is true you will get star');
+        document.write('<img src="https://image.pngaaa.com/369/1372369-middle.png" >'); 
+        break;
+        case "yes":alert('your answer is false you will not get star');}
+          
+     
+     console.log(fifth); 
